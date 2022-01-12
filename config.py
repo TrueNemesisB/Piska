4861e71dca932832e081c3acc8d20bc587823cdc
TrueNemesisB <97610529+TrueNemesisB@users.noreply.github.com>
2022-01-12 23:17:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,22 +32,4 @@ INTERVAL = 60
 TEMPLATES = {
    'about': 'Я настоящий. ⌛: $time' 
 
-from .module import Module
-import httpx
-
-
-class BrawlStarsTrophies(Module):
-    def __init__(self, tag, token):
-        self.hclient = httpx.AsyncClient(
-            base_url='https://api.brawlstars.com/v1/',
-            headers={
-                'Accept': 'application/json',
-                'authorization': 'Bearer ' + token
-            })
-
-        self.tag = tag
-
-    async def get(self) -> str:
-        response = (await self.hclient.get('players/%23' + self.tag)).json()
-        return response.get('trophies', 0)
 } 
