7ea1d6b6f07bbda4d065f71075db3194514a4d8e
TrueNemesisB <97610529+TrueNemesisB@users.noreply.github.com>
2022-01-12 21:37:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = 1890553
 API_HASH = 'c6f46b5330f5953e5201c3185337ed73'
 
 # https://developer.brawlstars.com/
-BRAWLSTARS_TOKEN = ''
+BRAWLSTARS_TOKEN = '298PJL20'
 
 # http://www.last.fm/api/account/create
 LASTFM_KEY = '50c4da48aa16885c9e91a6d417d601e7'  # usable example key
@@ -19,26 +19,14 @@ MODULES = {
     # Number of blocked accounts
     #'blocked': BlockedCount(),
 
-    # Number of channel or group members
-    'members': MembersCount('androidsmsbomber'),
-
-    # About of the entity
-    #'about': EntityInfo('wavecat'),
 
     # BrawlStars Trophies by tag
-    #'trophies': BrawlStarsTrophies('9PG9RCUVY', BRAWLSTARS_TOKEN),
+    #'trophies': BrawlStarsTrophies('298PJL20', BRAWLSTARS_TOKEN),
 
-    # Iterates strings
-    'first_name': Cycle('wavecat', 'retrocat'),
-
-    # LastFM now playing
-    #'music': LastFm(LASTFM_KEY, LASTFM_USERNAME, 'nothing is playing now')
 }
 
 INTERVAL = 60
 
 TEMPLATES = {
-    'about': '⌛: $time 💣: @androidsmsbomber ($members) 🔗: @CTRLIntelligence',
-    'first_name': '$first_name',
-    'last_name': None
+    'about': '⌛: $time
 }
