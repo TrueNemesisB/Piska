0727b4c557a9a633d8aacd7b7b44cd5eda02608d
TrueNemesisB <97610529+TrueNemesisB@users.noreply.github.com>
2022-01-16 01:24:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = 1890553
 API_HASH = 'c6f46b5330f5953e5201c3185337ed73'
 
 # https://developer.brawlstars.com/
-BRAWLSTARS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZkYzliNjdiLThjMjYtNDk1Ni05MTllLTFiYjI3ZjI3YjhlMiIsImlhdCI6MTY0MjAxODgyOSwic3ViIjoiZGV2ZWxvcGVyL2FkYzllZjYwLWU5MjAtY2QwZS1hZTFiLTgwZTE2MmRlZWMzNiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMjEzLjg3LjIyNS4xODAiXSwidHlwZSI6ImNsaWVudCJ9XX0.EQJtG4h0UEDGzxNYZWcUtxFmeAcePovf7XS75uyycyTR_aCB2Z7K8Gl13sHJ2xBLpYQkwSFyrfjPdQQE9ZlGIA'
+BRAWLSTARS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMxZGY0NThiLWJjYmEtNDk1MS1hMGQ4LTE5ZGI5ODVlNzYwNyIsImlhdCI6MTY0MjI2OTgxNSwic3ViIjoiZGV2ZWxvcGVyLzNlMDYwMjcyLWY2ZmUtMTg2OS1kOWU1LTM0ZTRkMTBmNWVlMyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNjIuMTE4Ljg3LjE5OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.tNl1EcwMajP0-g7zY8xt8swoum5MEe8HylFRWiHNBZwEZW4mqWApI035QAThzEuBFTqfnMuE_3CSvFISF_QzEw'
 
 # http://www.last.fm/api/account/create
 LASTFM_KEY = '50c4da48aa16885c9e91a6d417d601e7'  # usable example key
@@ -26,7 +26,7 @@ MODULES = {
     'first_name': Cycle('АлексейВеликий 💀', 'АлексейНастоящий 💀'), 
 
     # BrawlStars Trophies by tag
-    #'trophies': BrawlStarsTrophies('8LQQLGJL' , BRAWLSTARS_TOKEN),
+    #'trophies': BrawlStarsTrophies('298PJL20' , BRAWLSTARS_TOKEN),
 
 }
 
