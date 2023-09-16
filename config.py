1d40b4508a2ff8dda1eb2b58e796074a93a7e919
TrueNemesisB <97610529+TrueNemesisB@users.noreply.github.com>
2023-09-16 21:57:32 +0700

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = 1890553
 API_HASH = 'c6f46b5330f5953e5201c3185337ed73'
 
 # https://developer.brawlstars.com/
-BRAWLSTARS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU1OTMxYmZkLTExYzEtNDdiZS1hZGIwLWRjOGYxN2IzYTNhMyIsImlhdCI6MTY0MjQzMDgwNywic3ViIjoiZGV2ZWxvcGVyLzNlMDYwMjcyLWY2ZmUtMTg2OS1kOWU1LTM0ZTRkMTBmNWVlMyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNjIuMTE4Ljg1LjE1NCIsIjYyLjExOC44NS4xMzIiLCIyMTMuODcuMTI2LjgyIiwiNjIuMTE4Ljg3LjIzOSIsIjYyLjExOC44Ny4yMDgiXSwidHlwZSI6ImNsaWVudCJ9XX0.2nos7ShOJ-wfIoFzCrdyCcvUATz5EM5nM6USGMf1VXF7VTSnCvYfaaNACCnhVS97kKQTeEJAwSkeOtJ_Rrt6zw'
+BRAWLSTARS_TOKEN = ''
 
 # http://www.last.fm/api/account/create
 LASTFM_KEY = '50c4da48aa16885c9e91a6d417d601e7'  # usable example key
@@ -23,16 +23,16 @@ MODULES = {
     #'about': EntityInfo('True_Alexey'),
 
     # Iterates strings
-    'first_name': Cycle('АлексейВеликий 💀', 'АлексейНастоящий 💀'), 
+    'first_name': Cycle('АлексейВеликий', 'АлексейНастоящий'), 
 
     # BrawlStars Trophies by tag
-     'trophies': BrawlStarsTrophies('298PJL20' , BRAWLSTARS_TOKEN),
+     'trophies': BrawlStarsTrophies('' , BRAWLSTARS_TOKEN),
 
 }
 
-INTERVAL = 60
+INTERVAL = 20
 
 TEMPLATES = {
-   'about': 'Я настоящий. ⌛: $time ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ BrawlStars: $trophies 🏆', 
+   'about': 'Я настоящий.$time$time$time$time$time$time$time$time$time', 
    'first_name': '$first_name: $time'
 } 
